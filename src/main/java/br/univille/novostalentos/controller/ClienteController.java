6b2e56b486de71e6e2a3e039e6535ac4f9e8e611
WALTER SILVESTRE COAN <walter.s@univille.br>
2022-09-28 16:29:18 -0300

    WIP: implementação do cadastro de clientes

---
+++ src/main/java/br/univille/novostalentos/controller/ClienteController.java
@@ -0,0 +1,17 @@
+package br.univille.novostalentos.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/clientes")
+public class ClienteController {
+    
+    @GetMapping
+    public ModelAndView index(){
+
+        return null;
+    }
+}
