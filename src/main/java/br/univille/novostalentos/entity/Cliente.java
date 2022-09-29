ccf201e41bc1018e90e712766c2ce84cb1d4db97
WALTER SILVESTRE COAN <walter.s@univille.br>
2022-09-29 15:36:11 -0300

    WIP: integração com JPA e banco de dados

--- src/main/java/br/univille/novostalentos/entity/Cliente.java
+++ src/main/java/br/univille/novostalentos/entity/Cliente.java
@@ -1,7 +1,17 @@
 package br.univille.novostalentos.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @Column(length = 1000, nullable = false)
     private String nome;
     private String endereco;
     private String sexo;
