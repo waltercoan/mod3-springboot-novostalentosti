e755387e5a104782350905780e77e56850cbca40
WALTER SILVESTRE COAN <walter.s@univille.br>
2022-09-29 17:17:35 -0300

    WIP: integração com JPA e banco de dados

--- src/main/java/br/univille/novostalentos/controller/ClienteController.java
+++ src/main/java/br/univille/novostalentos/controller/ClienteController.java
@@ -1,20 +1,27 @@
 package br.univille.novostalentos.controller;
 
 import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import br.univille.novostalentos.entity.Cliente;
+import br.univille.novostalentos.repository.ClienteRepository;
 
 @Controller
 @RequestMapping("/clientes")
 public class ClienteController {
     
+    //esse codigo será removido - controlador nao deve chamar repositorio
+    @Autowired
+    private ClienteRepository repositorio;
+
     @GetMapping
     public ModelAndView index(){
 
-        ArrayList<Cliente> listaClientes = 
+        /*ArrayList<Cliente> listaClientes = 
             new ArrayList<>();
         
         var cliente1 = new Cliente();
@@ -31,7 +38,9 @@ public class ClienteController {
         cliente2.setEndereco("Rua lelelelel 200");
         cliente2.setSexo("Masculino");
 
-        listaClientes.add(cliente2);
+        listaClientes.add(cliente2);*/
+        
+        var listaClientes = repositorio.findAll();
 
         return new ModelAndView("cliente/index","listaClientes",listaClientes);
     }
--- src/main/java/br/univille/novostalentos/repository/ClienteRepository.java
+++ src/main/java/br/univille/novostalentos/repository/ClienteRepository.java
@@ -1,6 +1,9 @@
 package br.univille.novostalentos.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import br.univille.novostalentos.entity.Cliente;
@@ -8,5 +11,6 @@ import br.univille.novostalentos.entity.Cliente;
 @Repository
 public interface ClienteRepository 
     extends JpaRepository<Cliente,Long>{
-    
+    /*select * from cliente where nome like '%zinho%' */
+    List<Cliente> findByNomeIgnoreCaseContaining(@Param("nome") String nome);
 }
