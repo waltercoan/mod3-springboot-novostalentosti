37a49a4a5299dbd74a0c6d08c0c9a44b669362d0
WALTER SILVESTRE COAN <walter.s@univille.br>
2022-09-29 13:47:50 -0300

    WIP: integração com JPA e banco de dados

---
+++ src/main/java/br/univille/novostalentos/entity/Cliente.java
@@ -0,0 +1,35 @@
+package br.univille.novostalentos.entity;
+
+public class Cliente {
+    private long id;
+    private String nome;
+    private String endereco;
+    private String sexo;
+    
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    public String getSexo() {
+        return sexo;
+    }
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    
+}
