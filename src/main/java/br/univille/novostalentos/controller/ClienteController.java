2c144f4b7d2ba53df93eaebffa7c3055ee114c1d
WALTER SILVESTRE COAN <walter.s@univille.br>
2022-09-29 14:26:00 -0300

    WIP: integração com JPA e banco de dados

--- src/main/java/br/univille/novostalentos/controller/ClienteController.java
+++ src/main/java/br/univille/novostalentos/controller/ClienteController.java
@@ -1,9 +1,11 @@
 package br.univille.novostalentos.controller;
 
+import java.util.ArrayList;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import br.univille.novostalentos.entity.Cliente;
 
 @Controller
 @RequestMapping("/clientes")
@@ -12,6 +14,25 @@ public class ClienteController {
     @GetMapping
     public ModelAndView index(){
 
-        return new ModelAndView("cliente/index");
+        ArrayList<Cliente> listaClientes = 
+            new ArrayList<>();
+        
+        var cliente1 = new Cliente();
+        cliente1.setId(1);
+        cliente1.setNome("Zezinho");
+        cliente1.setEndereco("Rua lalalala 100");
+        cliente1.setSexo("Masculino");
+
+        listaClientes.add(cliente1);
+
+        var cliente2 = new Cliente();
+        cliente2.setId(2);
+        cliente2.setNome("Luizinho");
+        cliente2.setEndereco("Rua lelelelel 200");
+        cliente2.setSexo("Masculino");
+
+        listaClientes.add(cliente2);
+
+        return new ModelAndView("cliente/index","listaClientes",listaClientes);
     }
 }
