adbdb93db608baf10d00d632ecdfdcbd779a0ad5
WALTER SILVESTRE COAN <walter.s@univille.br>
2022-10-06 17:03:08 -0300

    WIP: função de excluir

---
+++ src/main/resources/static/script.js
@@ -0,0 +1,21 @@
+(function(){
+    $("#tabclientes").on("click",".js-delete", function(){
+        let botaoClicado  = $(this);
+        $("#btnsim").attr("data-id",botaoClicado.attr("data-id"));
+        $("#modalcliente").modal("show");
+    });
+
+    $("#btnsim").on("click",function(){
+        let botaoSim = $(this);
+        let id = botaoSim.attr("data-id");
+        $.ajax({
+            url: "/clientes/delete/" + id,
+            method: "GET",
+            success: function(){
+                window.location.href="/clientes";
+            }
+        });
+    });
+
+
+})();
