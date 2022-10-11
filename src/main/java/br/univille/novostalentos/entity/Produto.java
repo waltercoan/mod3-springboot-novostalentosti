eeb5dc301cb654c1933b75bb7501970a062ac364
WALTER SILVESTRE COAN <walter.s@univille.br>
2022-10-11 16:35:13 -0300

    WIP: cad prod - entidade

---
+++ src/main/java/br/univille/novostalentos/entity/Produto.java
@@ -0,0 +1,46 @@
+package br.univille.novostalentos.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(length = 1000)
+    private String nome;
+    @Column(length = 3000)
+    private String descricao;
+    private float valor;
+    
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public float getValor() {
+        return valor;
+    }
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+    
+
+}
