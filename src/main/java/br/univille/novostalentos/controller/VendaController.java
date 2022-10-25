c618f94a5b1ee14aa8d974eed5a59b8c2b614eb7
WALTER SILVESTRE COAN <walter.s@univille.br>
2022-10-25 16:14:35 -0300

    cadastro de vendas

--- src/main/java/br/univille/novostalentos/controller/VendaController.java
+++ src/main/java/br/univille/novostalentos/controller/VendaController.java
@@ -1,15 +1,30 @@
 package br.univille.novostalentos.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import br.univille.novostalentos.service.ClienteService;
+import br.univille.novostalentos.service.ProdutoService;
+import br.univille.novostalentos.service.VendaService;
 
 @Controller
 @RequestMapping("/vendas")
 public class VendaController {
 
+    @Autowired
+    private VendaService service;
+    @Autowired
+    private ClienteService clienteService;
+    @Autowired
+    private ProdutoService produtoService;
     
+    @GetMapping
     public ModelAndView index(){
-        return null;
+        var listaVendas = service.getAll();
+        return new ModelAndView("venda/index",
+                    "listaVendas",listaVendas);
     }
 }
