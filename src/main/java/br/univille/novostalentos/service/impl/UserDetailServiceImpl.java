1b713a555335f4991f33c91b9a50ee7aa8921453
WALTER SILVESTRE COAN <walter.s@univille.br>
2022-11-01 16:33:58 -0300

    WIP: Spring Security

---
+++ src/main/java/br/univille/novostalentos/service/impl/UserDetailServiceImpl.java
@@ -0,0 +1,31 @@
+package br.univille.novostalentos.service.impl;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import br.univille.novostalentos.repository.UsuarioRepository;
+import org.springframework.security.core.userdetails.User;
+@Service
+public class UserDetailServiceImpl 
+        implements UserDetailsService{
+    
+    @Autowired
+    private UsuarioRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) 
+            throws UsernameNotFoundException {
+        var umUsuario = repository.findByNome(username);
+
+        return new User(umUsuario.getNome(), 
+                        umUsuario.getSenha(),
+                        new ArrayList<>());
+    }
+    
+}
